af1c576e5df4651a4ed92712e3d9fb2adb3d1598
iamatharvak <kulkarni.atharva.1403@gmail.com>
2024-12-21 00:04:12 +0530

    Initial commit

---
+++ src/data/UserData.jsx
@@ -0,0 +1,38 @@
+export const UserData = {
+  name: "Atharva Kulkarni",
+  about:
+    "As a self-taught frontend developer, I specialize in creating dynamic, accessible websites that work seamlessly across any device. I consistently deliver creative solutions that improve user experiences. With proficiency in React.js, Redux,Nodejs, JavaScript, HTML5, and CSS, I excel in building high-quality, responsive web applications. My expertise with Git version control ensures efficient code management and smooth communication. I also have experience developing apps using Mini Program Studio frameworks and take pride in exceeding client expectations with innovative, high-caliber solutions.",
+  resumeUrl:
+    "https://drive.google.com/file/d/1LSVLVmJA_3fhWtZPc_d_pHnkXhL4s-Oc/view",
+  FooterLink: "https://www.linkedin.com/in/iamatharva-k/",
+  socialMedia: [
+    {
+      socialMediaName: "github",
+      url: "hhttps://github.com/iamatharvak",
+      icon: "AiFillGithub",
+    },
+    {
+      socialMediaName: "linkedin",
+      url: "https://www.linkedin.com/in/iamatharva-k/",
+      icon: "FaLinkedinIn",
+    },
+    {
+      socialMediaName: "twitter",
+      url: "https://twitter.com/iamatharvak_",
+      icon: "AiOutlineTwitter",
+    },
+  ],
+  typewriterOptions: {
+    strings: [
+      "Front End Developer",
+      "Full Stack Developer",
+      "Backend Devloper",
+      "React Js Developer",
+      "UI Developer",
+      "Javascript Developer",
+    ],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 40,
+  },
+};
